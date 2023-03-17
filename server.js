870920a632ed1714c5d4694822fae1144b428c6e
Damon-Jo <lunaticjky@gmail.com>
2023-03-17 00:41:53 -0700

    created write page for input and added API in server.js

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(8080, function(){
     console.log('listening on 8080')
 });
 
 
-// when access the '/pet' path, it shows 'hello world' msg
-app.get('/pet', function(req,res){
-    res.send('hello world');
-})
-
-app.get('/beauty', function(req,res){
-    res.send('hello beauty');
-})
-
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/write', function(req,res){
-    res.sendFile(__dirname + 'write.html')
+    res.sendFile(__dirname + '/write.html')
+})
+
+app.post('/add', function(req,res){
+    res.send('Message Send');
+    console.log(req.body.title);
 })
